781d594477537451753b11e729589763673e094d
cheetax <dmitriy.grebenev@gmail.com>
2018-05-21 17:16:59 +0300

    new

--- routers/users.js
+++ routers/users.js
@@ -2,8 +2,14 @@ var express = require('express'),
 router = express.Router();
 
 router.get('/', (req, res) => {
-    console.log(req)
-    res.send('User OK!')
+    console.log(req.session.user)
+    if (!req.session.val) req.session.val = 0
+    //window.postMessage({sender: 'iframeName'}, '*')
+    //res.json({status: 'User OK!'})
+    
+    req.session.user = 'test user:' + req.session.val
+    req.session.val += 1
+    res.redirect('back');
 })
 
 module.exports = router
