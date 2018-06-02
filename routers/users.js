ea466232f8d7b09eae96dc3b1d775d16cbf44401
cheetax <dmitriy.grebenev@gmail.com>
2018-06-02 19:48:56 +0300

    new

--- routers/users.js
+++ routers/users.js
@@ -1,9 +1,40 @@
 var express = require('express'),
-router = express.Router();
+    router = express.Router();
+
+var users = [
+    { name: 'Dmitriy' },
+    { name: 'Dmitriy' },
+    { name: 'Dmitriy' },
+    { name: 'Dmitriy' },
+    { name: 'Dmitriy' },
+    { name: 'Dmitriy' },
+    { name: 'Dmitriy' },
+    { name: 'Dmitriy' },
+    { name: 'Dmitriy' },
+    { name: 'Dmitriy' },
+    { name: 'Dmitriy' },
+    { name: 'Dmitriy' },
+    { name: 'Dmitriy' },
+    { name: 'Dmitriy' },
+    { name: 'Dmitriy' },
+    { name: 'Dmitriy' },
+    { name: 'Dmitriy' },
+    { name: 'Dmitriy' },
+    { name: 'Dmitriy' },
+    { name: 'Dmitriy' },
+    { name: 'Ivan' }
+]
+
+// var users = [    
+//     { name: 'Dmitriy' },
+//     { name: 'Dmitriy' },
+//     { name: 'Dmitriy' },
+//     { name: 'Ivan' }
+// ]
 
 router.post('/', (req, res) => {
     console.log(req.session)
-    if (req.session.isAuthorized) res.json([{ name: 'Dmitriy' }, {name: 'Ivan'}])
+    if (req.session.isAuthorized) res.json(users)
     else res.json({ status: false })
 })
 
