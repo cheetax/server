294e16a1778d86e6da9e19bef3ae39fd19d44761
cheetax <dmitriy.grebenev@gmail.com>
2018-06-19 13:31:01 +0300

    new

--- routers/users.js
+++ routers/users.js
@@ -1,11 +1,18 @@
 var express = require('express'),
     router = express.Router(),
     roles = require('./roles').roles;
+const crypto = require('crypto');
+var pbkdf2 = require('pbkdf2')
+
 
 const uid = () => "id" + Math.random().toString(16).slice(2);
 
-var users = [
-    {
+var salts = [];
+var users = [];
+
+(() => {
+    var _user = {};
+    _user = {
         id: uid(),
         firstName: 'Дмитрий',
         surName: 'Гребенев',
@@ -14,30 +21,39 @@ var users = [
         office: 'корпус 504 комната 248',
         phone: '52112',
         usePassword: false,
-        password: null,
+        password: '',
+        salt: '',
         roles: [
             roles.administrator.id,
             roles.customer.id
         ]
-    },
-    {
+    }
+    // salts.push({
+    //     id: _user.id,
+    //     salt: crypto.randomBytes(32)
+    // })
+    users.push(_user);
+    _user = {
         id: uid(),
         firstName: 'Михаил',
         surName: 'Стариков',
         post: 'Ведущий специалист',
         email: 'mikhail.starikov@gmail.com',
         office: 'корпус 504 комната 248',
-        phone: '52111',        
+        phone: '52111',
         usePassword: false,
         password: '',
+        salt: '',
         roles: [
             roles.customer.id
         ]
-    },
-]
-
-
-
+    };
+    // salts.push({
+    //     id: _user.id,
+    //     salt: crypto.randomBytes(32)
+    // })
+    users.push(_user);
+})()
 
 var user = (i) => {
     var _id = uid();
@@ -51,6 +67,7 @@ var user = (i) => {
         phone: '52112',
         usePassword: false,
         password: '',
+        salt: '',
         roles: [
             roles.administrator.id,
             roles.customer.id
@@ -62,6 +79,10 @@ var user = (i) => {
 var setUsers = () => {
     for (var i = 0; i <= 15000; i++) {
         users.push(user(i));
+        // salts.push({
+        //     id: user.id,
+        //     salt: crypto.randomBytes(32)
+        // })
     }
     console.log(users)
 };
@@ -89,8 +110,19 @@ router.get('/', (req, res) => {
 router.post('/', (req, res) => {
 
     if (req.session.isAuthorized) {
+
         var user = req.body;
+
         user.id = uid();
+
+        if (user.usePassword) {
+            var salt = crypto.randomBytes(32)
+            salts.push({
+                id: user.id,
+                salt: salt,
+            })
+            user.password = pbkdf2.pbkdf2Sync(user.password, salt, 1000, 32, 'sha256').toString();
+        }
         users.push(user);
         var response = {
             status: true,
@@ -107,9 +139,33 @@ router.put('/', (req, res) => {
 
     if (req.session.isAuthorized) {
         var user = req.body;
+
+
         users = users.map(item => {
             if (item.id !== user.id) return item;
-            else return user;
+            else {
+                if (user.usePassword) {
+                    if (user.password !== item.password) {
+                        var _salts = salts.filter(itemSalt => itemSalt.id == user.id);
+                        if (_salts.length == 0) {
+                            salt = crypto.randomBytes(32);
+                            salts.push({
+                                id: user.id,
+                                salt: salt,
+                            })
+                        }
+                        else salt = _salts[0].salt;
+
+                        //var pass = pbkdf2.pbkdf2Sync(user.password, salt, 1000, 32, 'sha256').toString()                      
+                        
+                        user.password = pbkdf2.pbkdf2Sync(user.password, salt, 1000, 32, 'sha256').toString();
+                    }
+                    else {
+                        
+                    }
+                }
+                return user;
+            }
         })
         var response = {
             status: true,
